cc3b4b0b16841f7e0eefd7d6cc9aee130a6078f6
Nagylla-Abreu <nagyllaabreu08@gmail.com>
2026-07-20 12:25:54 -0300

    exercícios

--- curso-em-video/mundo2/ex51.py
+++ curso-em-video/mundo2/ex51.py
@@ -1,8 +1,8 @@
-lop = 0
 p1 = int(input('Insira o primeiro termo da sua PA: '))
 razao = int(input('Insira a razão da sua PA: '))
 
-for c in range(11):
-    lop = c - 1
-    for pa in range(p1, lop, razao):
-        print(pa)
+termo = p1
+
+for i in range(10):
+    print(termo)
+    termo = termo + razao
