b0377eaf98dba8871c5bad0c4272e2e8bdace69e
Nagylla-Abreu <nagyllaabreu08@gmail.com>
2026-07-24 16:39:18 -0300

    questões

---
+++ exercicios-preengcompt/q20.py
@@ -0,0 +1,14 @@
+"""
+
+ Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem
+caso o valor seja inválido e continue pedindo até que o usuário informe um valor
+válido.
+
+"""
+
+nota = int(input('Insira uma nota de 0 a 10: '))
+while nota < 0 or nota > 10:
+    print('Nota inválida!')
+    nota = int(input('Insira uma nota de 0 a 10: '))   
+    i += 1
+print('Nota válida!')
