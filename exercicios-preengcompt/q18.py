af9cd951047026cc7a25cc6364c27581acb46e9e
Nagylla-Abreu <nagyllaabreu08@gmail.com>
2026-07-24 16:21:16 -0300

    questões

---
+++ exercicios-preengcompt/q18.py
@@ -0,0 +1,16 @@
+"""
+
+Faça dois códigos, um utilizando for e outro utilizando while para realizar a seguinte
+função:
+imprimir na tela 10 vezes a mensagem "Ola, UFC!".
+
+"""
+
+# for i in range(1, 11):
+#    print('Olá, UFC!')
+
+
+i = 1
+while i <= 10:
+    print('Olá, UFC!')
+    i += 1
