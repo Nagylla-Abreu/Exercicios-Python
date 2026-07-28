7e34a968067465922a3b3b081a2eccc4cd0a092e
Nagylla-Abreu <nagyllaabreu08@gmail.com>
2026-07-28 09:21:23 -0300

    exercícios

--- curso-em-video/mundo3/ex72.py
+++ curso-em-video/mundo3/ex72.py
@@ -1,12 +1,11 @@
 numext = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
-num = int(input('Digite um número entre 0 e 20: ')) 
+ 
 
 while True:   
-    if num <= 20:
-            break
-    elif num > 20 or num < 0:
-        print('Tente novamente. ', end='')
-        num = int(input('Digite um número entre 0 e 20: '))       
+    num = int(input('Digite um número entre 0 e 20: '))
+    if num >= 0 and num <= 20:
+        break
+    else:
+        print('Tente novamente. ', end='')     
 print(f'Você digitou o número {numext[num]}')
-
