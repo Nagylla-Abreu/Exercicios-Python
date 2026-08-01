372b11433bd189b93a0008b1a64ecc55df4ecca1
Nagylla-Abreu <nagyllaabreu08@gmail.com>
2026-07-31 21:13:52 -0300

    exercícios

--- curso-em-video/mundo2/ex51.py
+++ curso-em-video/mundo2/ex51.py
@@ -1,8 +1,13 @@
+print('=' * 20)
+print('10 TERMOS DE UMA PA')
+print('=' * 20)
+
 p1 = int(input('Insira o primeiro termo da sua PA: '))
 razao = int(input('Insira a razão da sua PA: '))
 
 termo = p1
 
 for i in range(10):
-    print(termo)
-    termo = termo + razao
+    print(f'{termo} > ', end='')
+    termo += razao
+print('ACABOU')
